39429b5a3a48137e1bed4ad4a9d4a6607e7012b2
taewoo.kim <taewoo.kim@infranicsmail.com>
2020-11-04 20:46:17 +0900

    Namespace Name 'all' exception handling and bugfix

--- src/main/java/org/paasta/container/platform/common/api/users/UsersRepository.java
+++ src/main/java/org/paasta/container/platform/common/api/users/UsersRepository.java
@@ -70,7 +70,7 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
     void deleteByCpNamespaceAndUserId(String namespace, String userId);
 
     @Query(value="SELECT * FROM cp_users WHERE cp_namespace = :namespace AND user_type ='"+Constants.AUTH_NAMESPACE_ADMIN+ "'limit 1;", nativeQuery=true)
-    Users findByCpNamespaceAndUserType(String namespace);
+    Users findByCpNamespaceAndUserType(@Param("namespace") String namespace);
 
     @Query(value="SELECT * FROM cp_users WHERE cluster_name = :cluster AND user_id = :userId AND cp_namespace NOT IN ('" + Constants.DEFAULT_NAMESPACE_NAME + "')", nativeQuery=true)
     List<Users> findAllByClusterNameAndUserId(@Param("cluster") String cluster, @Param("userId") String userId);
