49ac45f213d2c97d3147f37ebcfbe8fcdddf4464
hrjin <hyunlee3jdh@gmail.com>
2020-11-05 16:32:17 +0900

    add creationTimestamp column

--- src/main/java/org/paasta/container/platform/common/api/clusterResource/limitRanges/LimitRangesDefault.java
+++ src/main/java/org/paasta/container/platform/common/api/clusterResource/limitRanges/LimitRangesDefault.java
@@ -2,8 +2,12 @@ package org.paasta.container.platform.common.api.clusterResource.limitRanges;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.paasta.container.platform.common.api.common.Constants;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * LimitRanges Default Model 클래스
@@ -43,4 +47,14 @@ public class LimitRangesDefault {
 
     @Column(name = "default_limit")
     private String defaultLimit;
+
+    @Column(name = "created", nullable = false, updatable = false)
+    private String creationTimestamp;
+
+    @PrePersist
+    void preInsert() {
+        if (this.creationTimestamp == null) {
+            this.creationTimestamp = LocalDateTime.now(ZoneId.of(Constants.STRING_TIME_ZONE_ID)).format(DateTimeFormatter.ofPattern(Constants.STRING_DATE_TYPE));
+        }
+    }
 }
